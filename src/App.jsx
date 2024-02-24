be8c69ee74e6100504414886388f28ed4bc717aa
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-02-24 09:34:21 +0530

    donee

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
 
   return (
     <>
-    <HomeComponent/>
+    {/* <HomeComponent/> */}
     <QuizComponent/>
-    <ResultComponent/>
+    {/* <ResultComponent/> */}
     </>
   )
 }
